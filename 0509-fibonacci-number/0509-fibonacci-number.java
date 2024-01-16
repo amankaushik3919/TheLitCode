6b338e5946fcd9c9b298f179084b3c1a0d1c7ff7
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2024-01-16 23:17:26 +0530

    Time: 5 ms (47.62%), Space: 40.6 MB (8.80%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n==2){
+        if(n==2 || n==1){
             return 1;
         } else if(n==0) {
             return 0;
-        } else if(n==1){
-            return 1;
         }
         else{
             return fib(n-1)+fib(n-2);
