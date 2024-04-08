f7ac8a8955387654601e1cbb99555c876e4acd0e
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2024-04-08 16:57:38 +0530

    Time: 5 ms (52.93%) | Memory: 40.3 MB (48.01%) - LeetSync

---
+++ 3ba2ii/1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        if(n==2 || n==1){
+            return 1;
+        } else if(n==0) {
+            return 0;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+}
