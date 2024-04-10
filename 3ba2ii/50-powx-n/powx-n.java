4f6ad02e9508b6b16aa76d699cede878c71c1c60
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2024-04-10 23:45:21 +0530

    Delete 3ba2ii directory

--- 3ba2ii/50-powx-n/powx-n.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public double myPow(double x, int n) {
-        return Math.pow(x, n);
-    }
-}
--- 3ba2ii/69-sqrtx/sqrtx.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public int mySqrt(int x) {
-        return (int) Math.pow(x, 0.5);
-    }
-}
