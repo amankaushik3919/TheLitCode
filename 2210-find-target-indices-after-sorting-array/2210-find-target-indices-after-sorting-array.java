a96a81375e532755cf88493a4dee171448d6ad1b
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2024-06-10 10:26:06 -0600

    Time: 0 ms (100%), Space: 42.7 MB (96.36%) - LeetHub

---
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+                int count = 0;
+        int lessThanTarget = 0;
+        
+        // Count occurrences of target and numbers less than target
+        for (int num : nums) {
+            if (num == target) {
+                count++;
+            } else if (num < target) {
+                lessThanTarget++;
+            }
+        }
+        
+        // Generate the target indices
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            result.add(lessThanTarget + i);
+        }
+        
+        return result;
+    }
+}
