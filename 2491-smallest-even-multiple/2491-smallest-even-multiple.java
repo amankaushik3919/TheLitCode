8309e437b57345f7d59e5562ff02453e19b6e98c
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2024-09-20 12:00:54 +0530

    Time: 0 ms (100%), Space: 40 MB (92.71%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -2,7 +2,7 @@ class Solution {
     public int smallestEvenMultiple(int n) {
         int x = 1;
         while(x==x) {
-            if(x%2 == 0 && x%n == 0 && x%2 == 0) {
+            if(x%2 == 0 && x%n == 0) {
                 return x;
             }
             x+=1;
